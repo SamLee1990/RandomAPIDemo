d5081ae47d97b1a077e4ee518a7fadba62db971a
Sam Lee <sam1990790120@gmail.com>
2021-09-01 16:29:25 +0800

    Initial Commit

---
+++ RandomAPIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomAPIDemo
+//
+//  Created by 李世文 on 2021/9/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
